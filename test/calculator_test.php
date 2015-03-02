933a6415de47174de23aa4f55acb07e2428c9346
jimmy <jimmyjconnelly@gmail.com>
2015-03-02 10:45:51 +0000

    first commit

---
+++ test/calculator_test.php
@@ -0,0 +1,30 @@
+<?php
+
+
+require 'calculator.php';
+use App\Calculator;
+
+class CalculatorTest extends PHPUnit_Framework_TestCase
+{
+	public function testAdd()
+	{
+
+	$c = New Calculator;
+	$result = $c->add(15, 10);
+	$this->assertEquals(25, $result);
+	
+
+
+	}
+
+	public function testSubstract()
+	{
+	
+	$c = new Calculator;
+	$result = $c->subtract(50 , 20);
+	$this->assertEquals(30, $result);
+
+	}
+
+
+}
